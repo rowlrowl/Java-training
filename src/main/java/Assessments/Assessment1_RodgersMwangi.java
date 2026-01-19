2a6d41562005f90bc7b250caf6947228a63b0ecf
rowlrowl <rdgrsmwangi@gmail.com>
2026-01-19 21:16:19 +0300

    Assignment 1

---
+++ src/main/java/Assessments/Assessment1_RodgersMwangi.java
@@ -0,0 +1,29 @@
+//Name: Rodgers Mwangi
+
+//Quiz Problem 1: Variable Operations
+
+package Assessments;
+
+public class Assessment1_RodgersMwangi {
+    public static void main(String[] args){
+        int a=15;
+        int b=4;
+        int c=7;
+
+        int sum=a+b+c;  //Calculate Sum
+        int product=a*b; //Calculate product
+        int expression=(a+b)*c; //Calculate expression
+        int remainder=a%b; //Calculate remainder
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Expression Result: "+ expression);
+        System.out.println("Remainder: " + remainder);
+
+
+    }
+   
+
+    
+
+}
